deb3fc2326934e8b30d05efc24236436d3bc4c55
Danyalsmit <dk818552@gmail.com>
2023-11-05 13:26:25 +0500

    some work added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Login from "./Login";
-import Home from "./Home";
+import axios from "axios";
 
 function Signup() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [category, setCategory] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  
+    // You were missing the curly braces to define a block of code.
+    // Wrap your code like this:
+    const user = {
+      name,
+      number,
+      email,
+      category,
+      // password,
+    };
+  
+    axios
+      .post("http://localhost:8000/users", {
+        user,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -18,10 +50,10 @@ function Signup() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6">
           <div>
             <label
-              htmlFor="email"
+              htmlFor="name"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Full Name
@@ -29,18 +61,20 @@ function Signup() {
             <div className="mt-2">
               <input
                 id="name"
-                name="fullname"
-                placeholder=" Name"
+                name="name"
+                placeholder="Name"
                 type="text"
-                autoComplete="text"
+                autoComplete="name"
                 required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
           <div>
             <label
-              htmlFor="email"
+              htmlFor="number"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Mobile Number
@@ -48,15 +82,16 @@ function Signup() {
             <div className="mt-2">
               <input
                 id="number"
-                placeholder=" 0000-0000000"
                 name="number"
-                type="number"
-                optional
+                placeholder="0000-0000000"
+                type="text"
+                required
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
             <label
               htmlFor="email"
@@ -68,52 +103,72 @@ function Signup() {
               <input
                 id="email"
                 name="email"
-                placeholder=" Email"
+                placeholder="Email"
                 type="email"
                 autoComplete="email"
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                placeholder=" password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+            <div>
+              <label>Choose an option:</label>
+              <div>
+                <input
+                  type="radio"
+                  name="option"
+                  value="Company"
+                  checked={selectedOption === "Company"}
+                  onChange={() => setSelectedOption("Company")}
+                  required
+                />{" "}
+                Company
+                <input
+                  type="radio"
+                  name="option"
+                  value="Student"
+                  checked={selectedOption === "Student"}
+                  onChange={() => setSelectedOption("Student")}
+                  required
+                />{" "}
+                Student
+              </div>
             </div>
           </div>
-
+          {selectedOption === "Student" && (
+            <div>
+              <label>Select Category:</label>
+              <div>
+                <select
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">Select Category</option>
+                  <option value="Senior">Senior</option>
+                  <option value="Junior">Junior</option>
+                  <option value="Freshman">Freshman</option>
+                </select>
+              </div>
+            </div>
+          )}
           <div>
             <button
               type="submit"
-              onClick={Signup}
-              
+              onClick={handleFormSubmit}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              sign up
-            </button>   
+              Sign Up
+            </button>
           </div>
         </form>
         <div>
           <h2 className="text-black">
             Have an account{" "}
-            <Link className=" text-black-500 font-bold" to={"/login"}>
+            <Link className="text-black-500 font-bold" to="/login">
               Login
             </Link>
           </h2>
